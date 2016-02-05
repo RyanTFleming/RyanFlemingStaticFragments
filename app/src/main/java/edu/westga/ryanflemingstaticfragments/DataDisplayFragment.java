5ff541a5a1bad52546fb565dfbc790fd150316a0
Ryan Fleming <ryantfleming@aol.com>
2016-02-05 09:24:11 -0500

    Added code so the DataEntryFragment sends the data to the MainActivity which then displays the multiplication calculation in the DataDisplayFragment

--- app/src/main/java/edu/westga/ryanflemingstaticfragments/DataDisplayFragment.java
+++ app/src/main/java/edu/westga/ryanflemingstaticfragments/DataDisplayFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,17 +14,51 @@ import android.view.ViewGroup;
  */
 public class DataDisplayFragment extends Fragment {
 
+    private TextView displayProductTextView;
+    private double number1;
+    private double number2;
+    private double product;
 
     public DataDisplayFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_display, container, false);
+        View theView = inflater.inflate(R.layout.fragment_data_display, container, false);
+        this.displayProductTextView = (TextView) theView.findViewById(R.id.textView2);
+        return theView;
     }
 
+    /**
+     * Sets the number1 to the value of the first number sent to the fragment
+     * @param number1 - the first number
+     */
+    public void setNumber1(double number1) {
+        this.number1 = number1;
+    }
+
+    /**
+     * sets the number2 to the second number sent to the fragment
+     * @param number2 - the second number
+     */
+    public void setNumber2(double number2) {
+        this.number2 = number2;
+    }
+
+    /**
+     * calculates the product of the two numbers and sets the
+     * value to this.product.
+     */
+    public void multiply() {
+        this.product = number1 * number2;
+    }
+
+    /**
+     * sets the text of the TextView to the value of the product
+     */
+    public void displayProduct() {
+        this.displayProductTextView.setText(String.valueOf(this.product));
+    }
 }
--- app/src/main/java/edu/westga/ryanflemingstaticfragments/DataEntryFragment.java
+++ app/src/main/java/edu/westga/ryanflemingstaticfragments/DataEntryFragment.java
@@ -1,11 +1,15 @@
 package edu.westga.ryanflemingstaticfragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
@@ -13,17 +17,59 @@ import android.view.ViewGroup;
  */
 public class DataEntryFragment extends Fragment {
 
+    private TextView number1Text;
+    private TextView number2Text;
+    public static interface DataEntryListener {
+        void onDataEntry(double number1, double number2);
+    }
+    private DataEntryListener listener;
 
     public DataEntryFragment() {
         // Required empty public constructor
     }
 
-
+    /**
+     * called when the view is created.
+     *
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return theView
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_entry, container, false);
+        View theView = inflater.inflate(R.layout.fragment_data_entry, container, false);
+        this.number1Text = (EditText) theView.findViewById(R.id.editText);
+        this.number2Text = (EditText) theView.findViewById(R.id.editText2);
+
+        Button multiplyButton = (Button) theView.findViewById(R.id.button);
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                multiplyButtonClicked(view);
+            }
+        });
+
+        return theView;
     }
 
+    /**
+     * Action performed when the button is clicked
+     * @param view - the button
+     */
+    private void multiplyButtonClicked(View view) {
+        double number1 = Double.parseDouble(this.number1Text.getText().toString());
+        double number2 = Double.parseDouble((this.number2Text.getText().toString()));
+        this.listener.onDataEntry(number1, number2);
+    }
+
+    /**
+     * Called when fragment is activated
+     * @param context - the activity that contains the fragment
+     */
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.listener = (DataEntryListener) context;
+    }
 }
